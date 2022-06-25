f7506108470507dba7f80e5dd3ca81ce61206155
rapitwitterbots <rapitwitterbots@gmail.com>
2022-06-24 19:47:24 -0500

    added python version check

--- src/network_warden/__init__.py
+++ src/network_warden/__init__.py
@@ -1,1 +1,13 @@
-from network_warden import *
+"""Welcome to the Network Warden package, designed to help you monitor
+your network statistics in a digestible manner!
+"""
+
+# Enforce Python version check during package import.
+import sys
+
+if sys.version_info < (3, 8):
+    raise ImportError("Network Warden does not support Python < 3.8")
+
+# Packages may add whatever they like to this file, but
+# should keep this content at the top.
+#---------------------------------------------------------------
