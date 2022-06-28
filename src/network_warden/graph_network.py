f4885868b9b678ded948c4c73ba093b25f70286c
rapitwitterbots <rapitwitterbots@gmail.com>
2022-06-28 14:47:24 -0500

    rearrange classes to fix issue #18

--- src/network_warden/graph_network.py
+++ src/network_warden/graph_network.py
@@ -5,145 +5,79 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+import matplotlib.figure as figure
 
 import helpers
 
-
+    
 class Graph():
-    """Gets and prepares data from a csv and then plots the requested graphs.
+    """Interprets parameters and plots the requested graphs.
 
-    :return: essentially a Matplotlib.Axes.Subplot object
+    :param jitter: graph type, defaults to False
+    :type jitter: bool, optional
+    :param upload: graph type, defaults to False
+    :type upload: bool, optional
+    :param download: graph type, defaults to False
+    :type download: bool, optional
+    :return: essentially a matplotlib.Axes.Subplot object
     :rtype: Graph object
     """
-
+    
+    
     jitter_graph_params = {"xlabel": "Time", "ylabel": "Jitter (ms)",
                             "title": "Jitter over Time"}
     upload_graph_params = {"xlabel": "Time", "ylabel": "Upload (Mbps)",
                             "title": "Upload Speed over Time"}
     download_graph_params = {"xlabel": "Time", "ylabel": "Download (Mbps)",
                             "title": "Download Speed over Time"}
-    fig, axs = plt.subplots()
-    ip_address = "192.168.0.2"
-    csv_file_location = "/home/pi/network_monitor/network_monitor.csv"
-    remote_server_capability = False
-    remote_server_username = ""
-
-    def __init__(self, jitter=False, upload=False, download=False):
-        """Loads parameters and settings, then launches graph_selector().
-
-        :param jitter:Graph type, defaults to False
-        :type jitter: bool, optional
-        :param upload: Graph type, defaults to False
-        :type upload: bool, optional
-        :param download: Graph type, defaults to False
-        :type download: bool, optional
-        """
+    def __init__(self, jitter, upload, download):
         
-        self.jitter = jitter
-        self.upload = upload
-        self.download = download
-
-        self.load_settings()
-
-        data = self.prepare_data()
-        self.time = data[:,1]
-        self.ping = data[:,2]
-        self.jitter_data = data[:,3]
-        self.download_data = data[:,4]
-        self.upload_data = data[:,5]
-
-        self.graph_selector()
-
-    def load_settings(self):
-        """Pulls relevant config.toml settings.
-        """
-        configs = helpers.read_from_config()
-        self.remote_server_capability = configs["user"]["remote_server_capability"]
-        if self.remote_server_capability:
-            self.csv_file_location = configs["remote_servers"]["csv_location"]
-            self.ip_address = configs["remote_servers"]["ip_address"]
-            self.remote_server_username = configs["remote_servers"]["username"]
+        i = 0
+        graphed = {}
+        if jitter:
+            graphed["jitter"] = False
+            i += 1
+        if upload:
+            graphed["upload"] = False
+            i += 1
+        if download:
+            graphed["download"] = False
+            i += 1
+    
+        data = LineData(jitter=jitter, upload=upload, download=download)
+        
+        fig, axs = plt.subplots(i, sharex=True)
+        fig.set_size_inches(12, 9)
+        
+        plt.subplots_adjust(hspace=.3)
+        
+        if i > 1:
+            for j in range(0, i):
+                if jitter and graphed["jitter"] == False:
+                    self.my_plotter(axs[j], data.time_data, data.jitter_data,
+                                self.jitter_graph_params)
+                    graphed["jitter"] = True
+                    continue
+                if upload and graphed["upload"] == False:
+                    self.my_plotter(axs[j], data.time_data, data.upload_data,
+                                self.upload_graph_params)
+                    graphed["upload"] = True
+                    continue
+                if download and graphed["download"] == False:
+                    self.my_plotter(axs[j], data.time_data, data.download_data,
+                                self.download_graph_params)
+                    graphed["download"] = True
         else:
-            self.csv_file_location = configs["user"]["csv_location"]
-            self.ip_address = configs["user"]["ip_address"]
-
-    def graph_selector(self):
-        """Selects the appropriate graph from parameters
-
-        :return: Essentially a Matplotlib.Axes.Subplot object
-        :rtype: Graph object
-        """
-        
-        if self.jitter and not self.upload and not self.download:
-            self.fig, self.ax = plt.subplots()
-            self.my_plotter(self.ax, self.time, self.jitter_data, 
-                            self.jitter_graph_params)
-        
-        elif self.upload and not self.jitter and not self.download:
-            self.fig, self.ax = plt.subplots()
-            self.my_plotter(self.ax, self.time, self.upload_data, 
+            if jitter:
+                    self.my_plotter(axs, data.time_data, data.jitter_data,
+                                self.jitter_graph_params)
+            if upload:
+                self.my_plotter(axs, data.time_data, data.upload_data,
                             self.upload_graph_params)
-        
-        elif self.download and not self.jitter and not self.upload:
-            self.fig, self.ax = plt.subplots()
-            self.my_plotter(self.ax, self.time, self.download_data, 
+            if download:
+                self.my_plotter(axs, data.time_data, data.download_data,
                             self.download_graph_params)
-        
-        elif self.upload and self.jitter and not self.download:
-            self.fig, self.axs = plt.subplots(2, sharex=True)
-            self.my_plotter(self.axs[0], self.time, self.jitter_data, 
-                            self.jitter_graph_params)
-            self.my_plotter(self.axs[1], self.time, self.upload_data, 
-                            self.upload_graph_params)
-
-        elif self.download and self.jitter and not self.upload:
-            self.fig, self.axs = plt.subplots(2, sharex=True)
-            self.my_plotter(self.axs[0], self.time, self.jitter_data, 
-                            self.jitter_graph_params)
-            self.my_plotter(self.axs[1], self.time, self.download_data, 
-                            self.download_graph_params)
-        
-        elif self.upload and self.download and not self.jitter:
-            self.fig, self.axs = plt.subplots(2, sharex=True, sharey=True)
-            self.my_plotter(self.axs[0], self.time, self.upload_data, 
-                            self.upload_graph_params)
-            self.my_plotter(self.axs[1], self.time, self.download_data, 
-                            self.download_graph_params)
-        
-        elif self.upload and self.download and self.jitter:
-            self.fig, self.axs = plt.subplots(3, sharex=True)
-            self.my_plotter(self.axs[0], self.time, self.download_data, 
-                            self.download_graph_params)
-            self.my_plotter(self.axs[1], self.time, self.upload_data, 
-                            self.upload_graph_params)
-            self.my_plotter(self.axs[2], self.time, self.jitter_data, 
-                            self.jitter_graph_params)
-
-        else:
-            print("Could not select any of the graph options available!")
-            raise ValueError("No graph type selected!")
-            
-        
-        self.fig.set_figheight(7)
-        self.fig.set_figwidth(10)
-        plt.subplots_adjust(hspace=.5)
-
-        return self.fig, self.axs
-
-    def prepare_data(self):
-        """Calls three helper.py functions to prepare data for graphing.
-
-        :return: contains data to graph
-        :rtype: np.array
-        """
-        
-        if self.remote_server_capability:
-            self.get_csv()
-        
-        df = self.panda_csv()
-        data = self.convert_to_np_array(df)
-        return data
-
+    
     def my_plotter(self, ax, data1, data2, param_dict={}):
         """Creates a line based upon input x, y
 
@@ -171,6 +105,77 @@ class Graph():
 
         return out
 
+    
+
+
+class LineData():
+    """Includes all methods and objects for the data which will be graphed.
+
+    :return: LineData object which wraps all data together.
+    :rtype: LineData object
+    """
+    
+    
+    time_data = []
+    jitter_data = []
+    upload_data = []
+    download_data = []
+    
+    def __init__(self, jitter=False, upload=False, download=False):
+        """Loads parameters and settings, then launches graph_selector().
+
+        :param jitter:Graph type, defaults to False
+        :type jitter: bool, optional
+        :param upload: Graph type, defaults to False
+        :type upload: bool, optional
+        :param download: Graph type, defaults to False
+        :type download: bool, optional
+        """
+        
+        self.jitter = jitter
+        self.upload = upload
+        self.download = download
+
+        self.load_settings()
+
+        data = self.prepare_data()
+        self.time_data = data[:,1]
+        if self.jitter:
+            self.jitter_data = data[:,3]
+        if self.download:
+            self.download_data = data[:,4]
+        if self.upload:
+            self.upload_data = data[:,5]
+            
+        
+        
+    def load_settings(self):
+        """Pulls relevant config.toml settings.
+        """
+        configs = helpers.read_from_config()
+        self.remote_server_capability = configs["user"]["remote_server_capability"]
+        if self.remote_server_capability:
+            self.csv_file_location = configs["remote_servers"]["csv_location"]
+            self.ip_address = configs["remote_servers"]["ip_address"]
+            self.remote_server_username = configs["remote_servers"]["username"]
+        else:
+            self.csv_file_location = configs["user"]["csv_location"]
+            self.ip_address = configs["user"]["ip_address"]
+    
+    def prepare_data(self):
+        """Calls three helper.py functions to prepare data for graphing.
+
+        :return: contains data to graph
+        :rtype: np.array
+        """
+        
+        if self.remote_server_capability:
+            self.get_csv()
+        
+        df = self.panda_csv()
+        data = self.convert_to_np_array(df)
+        return data
+    
     def get_csv(self):
         """Establish connection with raspi and pull csv
         """
@@ -232,9 +237,11 @@ class Graph():
 
         np_array = np.asarray(df)
         return np_array
+    
+    
 
 def main(jitter=False, upload=False, download=False):
-    """Entry point for Graph class
+    """Entry point for graph_network.py
 
     :param jitter: graph type, defaults to False
     :type jitter: bool, optional
@@ -242,12 +249,15 @@ def main(jitter=False, upload=False, download=False):
     :type upload: bool, optional
     :param download: graph type, defaults to False
     :type download: bool, optional
-    :return: essentially a matplotlib.Axes.Subplot object
-    :rtype: Graph objec
     """
+    
+    
     
     graph = Graph(jitter, upload, download)
     return graph
-
+        
+    
 if __name__ == '__main__':
-    main()
+    main(download=True, jitter=True)
+    
+    plt.show()
